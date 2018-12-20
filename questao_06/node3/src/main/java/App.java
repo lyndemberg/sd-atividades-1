c6883ab24f9118fee7bdabe0adc19335929e5eb9
lyndemberg <lyndembergbatista@outlook.com>
2018-12-20 20:49:55 -0300

    finalizando

--- questao_06/node3/src/main/java/App.java
+++ questao_06/node3/src/main/java/App.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        File dir = new File("../tmp"    );
+        File dir = new File("../tmp");
         if(dir.exists()){
             //CRIAR UM ARQUIVO DE TESTE PARA GERAR NOTIFICAÇÃO
             File file1 = new File("../tmp/teste.txt");
